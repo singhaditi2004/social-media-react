ce4645c3bc9b9a848a832b113c4e14a7cee9cd77
Aditi Singh <aditisinghbais_cse21@ggct.co.in>
2024-09-10 07:46:51 +0530

    UI updated

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,15 +1,17 @@
-const Card = () => {
+const Card = ({ post }) => {
   return (
-    <div className="card" style={{ width: "18rem" }}>
+    <div className="card post-card" style={{ width: "18rem" }}>
       <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card&apos;s content.
-        </p>
-        <a href="#" className="btn btn-primary">
-          Go somewhere
-        </a>
+        <h5 className="card-title">
+          {post.title}
+          <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+            {post.react}
+          </span>
+        </h5>
+        <p className="card-text">{post.content}</p>
+        {post.tags.map((tag) => (
+          <span className="badge text-bg-primary hashtag">{tag}</span>
+        ))}
       </div>
     </div>
   );
